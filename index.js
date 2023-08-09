cbc7a13b50b8efb9809ce2fd8b46232c9e9230c4
Duke <dukemorise33@gmail.com>
2023-08-08 21:18:57 -0700

    completed closure, scope, etc. phase-1 functions lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity='roller-skate'){
+    const returnStatement = `This Saturday, I want to ${activity}!`;
+    return returnStatement;
+}
+
+function mondayWork(activity='go to the office'){
+    const returnStatement = `This Monday, I will ${activity}.`;
+    return returnStatement;
+}
+
+function wrapAdjective(adj){
+    const youAre = 'You are';
+    return function (baseWord){
+        return `${youAre} ${adj}${baseWord}${adj}!`;
+    }
+}
+
